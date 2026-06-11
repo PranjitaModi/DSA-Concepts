a734d8f3228ea1a0f92e5fa1519fcc1af67832fc
PranjitaModi <modipranjita@gmail.com>
2026-06-11 15:49:30 +0530

    Added more

---
+++ Stacks/reverse_string_usingStack.cpp
@@ -0,0 +1,15 @@
+//leetcode
+//https://leetcode.com/problems/reverse-string
+class Solution {
+public:
+    void reverseString(vector<char>&s) {
+        stack<char>st;
+        for(int i=0;i<s.size();i++){
+            char ch=s[i];
+            st.push(ch);}
+        for(int i=0;i<s.size();i++){
+            s[i]=st.top();
+            st.pop();
+        }
+    }
+};
