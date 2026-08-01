a0060287ac1ecdb571801333090debd633feb239
PranjitaModi <modipranjita@gmail.com>
2026-08-01 17:23:15 +0530

    updated

---
+++ Strings/Maximum_Nesting_Depth_of_the_Parentheses.cpp
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses
+
+class Solution {
+public:
+    int maxDepth(string s) {
+         int maxDepth = 0;
+         int currDepth = 0;
+
+       for (char ch:s){
+        if(ch=='('){
+            currDepth++;
+            maxDepth=max(maxDepth,currDepth);
+        }
+        else if(ch==')'){
+            currDepth--;
+        }
+       }
+       return maxDepth;
+    }
+};
